a68ca5a0c578d6fb5dfd0ea81a14f40e8b10944e
razvn <745873+razvn@users.noreply.github.com>
2024-12-02 14:19:40 +0100

    refactor: day 2 clean up

--- src/day02/Day02.kt
+++ src/day02/Day02.kt
@@ -15,27 +15,18 @@ fun main() {
 }
 
 object Day02 : Day<Int, Int, List<List<Int>>> {
-    override fun decodeData(input: List<String>): List<List<Int>> {
-        return input.map { it.split(" ").map(String::toInt) }
-    }
 
-    override fun part1(input: List<String>): Int {
-        val data = decodeData(input)
-        val inc = data.count { isIncrease(it) }
-        val dec = data.count { isIncrease(it.reversed()) }
-        return inc + dec
-    }
+    override fun decodeData(input: List<String>) = input.map { it.split(" ").map(String::toInt) }
 
-    override fun part2(input: List<String>): Int {
-        val data = decodeData(input)
-        val inc = data.count { isIncreaseTolerate(it) }
-        val dec = data.count { isIncreaseTolerate(it.reversed()) }
-        return inc + dec
-    }
+    override fun part1(input: List<String>) = decodeData(input).count(::isValidPart1)
 
-    private fun isIncrease(list: List<Int>): Boolean {
-        return list.zipWithNext().all { (a, b) -> b - a in 1..3 }
-    }
+    override fun part2(input: List<String>) = decodeData(input).count(::isValidPart2)
+
+    private fun isValidPart1(list: List<Int>) = isIncrease(list) || isIncrease(list.reversed())
+
+    private fun isValidPart2(list: List<Int>) = isIncreaseTolerate(list) || isIncreaseTolerate(list.reversed())
+
+    private fun isIncrease(list: List<Int>) = list.zipWithNext().all { (a, b) -> b - a in 1..3 }
 
     private fun isIncreaseTolerate(list: List<Int>): Boolean {
         for (i in 0..< list.size - 1) {
