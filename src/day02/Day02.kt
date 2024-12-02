f67c14b2bcf9cfb72719b416024e9dceacb9691e
razvn <745873+razvn@users.noreply.github.com>
2024-12-02 14:04:02 +0100

    refactor: day 2 remove decrease -> using increase on reverse :)

--- src/day02/Day02.kt
+++ src/day02/Day02.kt
@@ -22,36 +22,19 @@ object Day02 : Day<Int, Int, List<List<Int>>> {
     override fun part1(input: List<String>): Int {
         val data = decodeData(input)
         val inc = data.count { isIncrease(it) }
-        val dec = data.count { isDecrease(it) }
+        val dec = data.count { isIncrease(it.reversed()) }
         return inc + dec
-    }
-
-    private fun isDecrease(list: List<Int>): Boolean {
-        return list.zipWithNext().all { (a, b) -> a - b in 1..3 }
-    }
-
-    private fun isIncrease(list: List<Int>): Boolean {
-        return list.zipWithNext().all { (a, b) -> b - a in 1..3 }
     }
 
     override fun part2(input: List<String>): Int {
         val data = decodeData(input)
         val inc = data.count { isIncreaseTolerate(it) }
-        val dec = data.count { isDecreaseTolerate(it) }
+        val dec = data.count { isIncreaseTolerate(it.reversed()) }
         return inc + dec
     }
 
-    private fun isDecreaseTolerate(list: List<Int>): Boolean {
-        for (i in 0..<list.size - 1) {
-            val diff = list[i] - list[i + 1]
-            if (diff in 1..3) {
-                continue
-            } else {
-                return isDecrease(list.toMutableList().apply { removeAt(i) })
-                        || isDecrease(list.toMutableList().apply { removeAt(i + 1) })
-            }
-        }
-        return true
+    private fun isIncrease(list: List<Int>): Boolean {
+        return list.zipWithNext().all { (a, b) -> b - a in 1..3 }
     }
 
     private fun isIncreaseTolerate(list: List<Int>): Boolean {
